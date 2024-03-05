287f5146784013381fb495babcec7bb535d48880
liliia_vintiuk <rudapanda2016@gmail.com>
2024-03-05 14:32:28 +0200

    math round

--- uzd/to-fixed.js
+++ uzd/to-fixed.js
@@ -1,3 +1,4 @@
+
 function toFixed(n, f = 0) {
     if (f < 0) {
         return 'ERROR';
@@ -5,10 +6,10 @@ function toFixed(n, f = 0) {
 
     const text = '' + n;
     const parts = text.split('.');
-
-    const sveikojiDalis = parts[0];
+    const partsRounds = mathRuond(parts)
+    const sveikojiDalis = partsRounds[0];
     // const desimtaineDalis = parts[1] || ''; // || jei reiksme falsy ('', 0, false, undefined, null)
-    const desimtaineDalis = parts[1] ?? ''; // ?? jei reiksme neegzistuoja
+    const desimtaineDalis = partsRounds[1] ?? ''; // ?? jei reiksme neegzistuoja
 
     if (f === 0) {
         return sveikojiDalis;
@@ -16,8 +17,21 @@ function toFixed(n, f = 0) {
 
     return sveikojiDalis + '.' + (desimtaineDalis + '0'.repeat(f)).slice(0, f);
 }
+/*function toFixed2(n, f = 0){
+const desimtaineDalis = n > 0 ? n % 1 : -n % 1;
+const sveikojiDalis = n - desimtaineDalis;
+//console.log(n, ':', sveikojiDalis );
 
-console.log(toFixed(3, 0), '-->', '3');
+let result = ' ';
+if (f === 0){
+   return result ;
+}
+if(f > 0){
+    result += ('' + desimtaineDalis).slice(1);
+}
+return result;
+}*/
+console.log(toFixed(30.2, 0), '-->', '3');
 console.log(toFixed(-3, 0), '-->', '-3');
 console.log(toFixed(3.14, 0), '-->', '3');
 console.log(toFixed(-3.14, 0), '-->', '-3');
@@ -40,5 +54,19 @@ console.log(toFixed(-3.1, 2), '-->', '-3.10');
 console.log(toFixed(3, 3), '-->', '3.000');
 console.log(toFixed(-3, 3), '-->', '-3.000');
 
+console.log(toFixed(6.2568963, 4), '-->', '6.2569');
+
 console.log(toFixed(3.14159, 3), '-->', '3.142');
 console.log(toFixed(-3.14159, 3), '-->', '-3.142');
+
+function mathRuond(n){
+
+    let hh = n[1]?.split('') || [];
+    let h1 = parseFloat(hh.at(-1));
+    if(h1 >= 5){
+        return [n[0], (parseFloat(n[1].slice(0, -1))+1).toString()]
+    }else{
+        return n;
+    }
+
+}
